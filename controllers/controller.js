c99553191f53029416aaf86226db2abb6f67c6e7
Gabriel Araujo <el1j4h_morp@proton.me>
2024-04-09 20:18:44 -0300

    get roupas pelo id

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,9 +1,25 @@
 var path = require("path");
+const data = require("./dados.json");
 
 const controller = {};
 
+// retorna dados.json na raiz
 controller.getAllRoupas = (req, res) => {
   res.json(require("./dados.json"));
+};
+
+// retorna a roupa correspondente ao id
+controller.getRoupasById = (req, res) => {
+  const id = parseInt(req.params.id);
+  item = data.find((item) => item.id === id);
+
+  if (item) {
+    res.status(200).send(item);
+  } else {
+    res
+      .status(404)
+      .sendFile(path.resolve(__dirname + "/../views/not-found/index.html"));
+  }
 };
 
 module.exports = controller;
--- routes/routes.js
+++ routes/routes.js
@@ -4,8 +4,8 @@ const controller = require("../controllers/controller");
 const routes = express.Router();
 
 routes.get("/", controller.getAllRoupas);
+routes.get("/:id", controller.getRoupasById);
 /*
-routes.get("/pessoas/:id", controller.getPessoaById);
 routes.post("/pessoas", controller.createPessoa);
 routes.put("/pessoas/:id", controller.updatePessoa);
 routes.delete("/pessoas/:id", controller.deletePessoa);
